61df9310a9eab03beaf39ec994e4252aa859f5f8
caiorolim <caiorolim.squadra@parceiro.afya.com.br>
2025-03-14 09:28:42 -0300

    Aula 3: Configurando envio de emails na nossa aplicação

--- services/index.js
+++ services/index.js
@@ -4,6 +4,7 @@ module.exports = {
     logaUsuario: require('./loga-usuario'),
     checaSaldo: require('./checa-saldo'),
     trocaMoedas: require('./troca-moedas'),
+    enviaEmail: require('./envia-email'),
     sacaCryptos: require('./saca-crypto'),
     geraPnl: require('./gera-pnl'),
     cancelarDeposito: require('./cancela-deposito'),
