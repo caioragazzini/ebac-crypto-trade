2c137ff9db84a06f46584fa68331dc3343adc5d7
caiorolim <caiorolim.squadra@parceiro.afya.com.br>
2025-03-28 15:33:54 -0300

    Aula 6: Dominando mocks

--- tests/unit/services/busca-cotacoes.test.js
+++ tests/unit/services/busca-cotacoes.test.js
@@ -23,6 +23,56 @@ describe('buscaCotacoesOnLine', ()=> {
            
         
     });
-    
+    describe('se a rota de cotações retorna cotações validas', ()=> {
+        let resposta;
+        beforeEach(async() => {
+            nock(process.env.COIN_MARKETCAP_URL)
+            .get('/v2/cryptocurrency/quotes/latest?symbol=BTC,ETH,BNB,XRP,ADA,SOL&convert=BRL')
+            .reply(200, {
+                data: {
+                    BTC: [{
+                        symbol:'BTC',
+                        quote: {
+                            BRL: {
+                                price: 100500
+                            }
+                        }
+                    }],
+                    ETH: [{
+                        symbol:'ETH',
+                        quote: {
+                            BRL: {
+                                price: 12000
+                            }
+                        }
+                    }],
+
+                }
+            });
+
+            resposta = await buscaCotacoesOnline(); 
+        });
+
+        test('ele retorna uma lista de cotações', async()=> {
+
+            
+            expect(resposta.length).toBe(2);
+        });
+
+        test('ele a cotação corretamente', async()=> {
+           
+            expect(resposta[0].valor).toBe(100500);
+            expect(resposta[0].moeda).toBe('BTC');
+            expect(resposta[1].valor).toBe(12000);
+            expect(resposta[1].moeda).toBe('ETH');
+        });
+
+        test('ele adiciona hoje como a data da cotações', async()=> {
+            
+            expect(resposta[0].data.getDate()).toBe((new Date()).getDate());
+            expect(resposta[0].data.getDate()).toBe((new Date()).getDate());
+        });
+
+    });
    
 }); 
